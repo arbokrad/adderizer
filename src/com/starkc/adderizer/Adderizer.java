79e13260814673e0a2c77edb2086371e91c042c7
arbokrad <cstark1+git@gmail.com>
2015-11-09 16:24:16 -0500

    adding comments

--- src/com/starkc/adderizer/Adderizer.java
+++ src/com/starkc/adderizer/Adderizer.java
@@ -4,14 +4,17 @@ public class Adderizer {
 
 	public static void main(String[] args ) {
 	
+		// sanity check on command line args
 		if( args == null || args.length != 2 ) {
 			System.out.println( "Invalid parameters, aborting..." );
 			return;
 		}
 		
+		// grab both params
 		Integer num1 = Integer.parseInt( args[0] );
 		Integer num2 = Integer.parseInt( args[1] );
 		
+		// add and print the results
 		System.out.println( num1 + num2 );
 	}
 }
